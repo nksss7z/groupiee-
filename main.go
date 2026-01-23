444b3c1bc1247897928d5887859555584e51ef72
knilaksh <nilakshan.konesalingam@ynov.com>
2026-01-23 01:08:48 +0100

    groupie

--- main.go
+++ main.go
@@ -1,29 +1,12 @@
 package main
 
-import (
-	"flag"
-	"log"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"groupieee/api"
-	"groupieee/server"
-)
+import ("flag";"log";"os";"os/signal";"syscall";"groupieee/api";"groupieee/server")
 
 func main() {
 	port := flag.String("port", ":8080", "Port du serveur")
 	flag.Parse()
-
-	if err := api.LoadData(); err != nil {
-		log.Fatal(err)
-	}
-
-	go func() {
-		<-make(chan os.Signal, 1)
-		signal.Notify(make(chan os.Signal, 1), syscall.SIGINT, syscall.SIGTERM)
-		os.Exit(0)
-	}()
-
+	if err := api.LoadData(); err != nil { log.Fatal(err) }
+	sig := make(chan os.Signal, 1); signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	go func() { <-sig; os.Exit(0) }()
 	log.Fatal(server.Start(*port))
 }
