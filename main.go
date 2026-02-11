61960426b13d5ca732b88f123be9d6b72d6e0e06
Dylan-MOUTY <dylanmouty77@gmail.com>
2026-02-11 11:30:45 +0100

    test serveur

--- main.go
+++ main.go
@@ -1,12 +1,43 @@
 package main
 
-import ("flag";"log";"os";"os/signal";"syscall";"groupieee/api";"groupieee/server")
+import (
+	"flag"
+	"groupieee/api"
+	"groupieee/server"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	port := flag.String("port", ":8080", "Port du serveur")
+
 	flag.Parse()
-	if err := api.LoadData(); err != nil { log.Fatal(err) }
-	sig := make(chan os.Signal, 1); signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	go func() { <-sig; os.Exit(0) }()
-	log.Fatal(server.Start(*port))
+
+	// 🔥 Récupérer le port Scalingo
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // pour le local uniquement
+	}
+
+	// Initialisation API
+	api := api.New()
+
+	// Démarrage serveur
+	srv := server.New(api, port)
+
+	go func() {
+		log.Println("Server running on port", port)
+		if err := srv.Start(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Gestion arrêt propre
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+	<-stop
+
+	log.Println("Shutting down server...")
+	srv.Stop()
 }
